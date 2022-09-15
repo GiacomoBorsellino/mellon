abeddc5742dd0a364295de6a4e026c72df5bdd55
Giacomo Borsellino <giacomo.borsellino@avrgroup.it>
2022-09-15 17:20:40 +0200

    fin page

--- js/index.js
+++ js/index.js
@@ -3,7 +3,27 @@ function sound() {
     audio.play();
 }
 
-function soundLock() {
+function unLock() {
     let audio = new Audio('../sound/lockSound.mp3');
     audio.play();
-}
+
+    setTimeout(() => {
+        document.getElementById('white').classList.add('whiteCover');
+        document.getElementById('white').classList.remove('whiteUncover');
+    }, 0);
+
+    setTimeout(() => {
+        document.getElementById('white').classList.remove('whiteCover');
+        document.getElementById('white').classList.add('whiteRemover');
+        document.body.getElementsByClassName('chest-close')[0].style.display = "none";
+        document.body.getElementsByClassName('chest-open')[0].style.display = "block";
+        document.body.getElementsByClassName('lock')[0].style.display = "none";
+        document.body.getElementsByClassName('leaf')[0].style.display = "block";
+    }, 1000);
+
+    setTimeout(() => {
+        document.getElementById('white').classList.remove('whiteRemover');
+        document.getElementById('white').classList.add('whiteUncover');
+    }, 2000);
+
+};

6f99e746d7a5cf3aeff3c1278d3aa2c6db78ecc8
Giacomo Borsellino <giacomo.borsellino@avrgroup.it>
2022-09-15 09:20:42 +0200

    new page

--- js/index.js
+++ js/index.js
@@ -2,3 +2,8 @@ function sound() {
     let audio = new Audio('../sound/bookSound.mp3');
     audio.play();
 }
+
+function soundLock() {
+    let audio = new Audio('../sound/lockSound.mp3');
+    audio.play();
+}
